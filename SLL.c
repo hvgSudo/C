6a466f153bacf94e550c15c2f2e196a8506f5017
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-06-02 20:57:40 +0530

    Modified

---
+++ SLL.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int insert(struct Node*, int); 
+
+// Main method
+int main() {
+    struct Node* head;
+    return 0;
+}
